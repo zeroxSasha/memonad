2738feb2a59ec8341591888b5d1939f436dd38b4
Sasha <121317845+lxudrr@users.noreply.github.com>
2024-05-04 17:40:46 +0200

    chore: eliminate push notifications

--- scripts/permission.js
+++ scripts/permission.js
@@ -1,10 +1,1 @@
-const button = document.querySelector(".main__button");
-
-button.addEventListener("click", () => {
-    Notification.requestPermission().then(perm => {
-        if (perm === "granted") {
-            new Notification("Welcome <3");
-        }
-    })
-})
 
