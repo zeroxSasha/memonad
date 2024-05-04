438427b99343d51c1c690b0507c7880a828b2ca7
Sasha <lxudrr@gmail.com>
2024-05-04 13:32:40 +0200

    feat: add a push notification

--- scripts/permission.js
+++ scripts/permission.js
@@ -1,6 +1,10 @@
 const button = document.querySelector(".main__button");
 
 button.addEventListener("click", () => {
-    Notification.requestPermission()
+    Notification.requestPermission().then(perm => {
+        if (perm === "granted") {
+            new Notification("Welcome <3");
+        }
+    })
 })
 
